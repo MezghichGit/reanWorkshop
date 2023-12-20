b22d9797dab615d2ee1f208dfd845aa4c8889a21
moahmed amine mezghich <git@mezghich.com>
2023-12-20 16:50:08 +0100

    amsMobile - part 1

---
+++ amsMobile/App.js
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
+import SignInScreen from './screens/SignInScreen';
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <SignInScreen/>  
+    </SafeAreaView>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
