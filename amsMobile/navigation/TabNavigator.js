5f831ad64781fb52f02df941be78758a97de37ce
moahmed amine mezghich <git@mezghich.com>
2023-12-21 14:40:33 +0100

    add de provider OK

--- amsMobile/navigation/TabNavigator.js
+++ amsMobile/navigation/TabNavigator.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native'
-import {createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import NewScreen from '../screens/NewScreen';
@@ -13,48 +13,48 @@ const Stack = createNativeStackNavigator();
 
 const HomeStack = () => (
     <Stack.Navigator>
- <Stack.Screen name="List Providers" component={HomeScreen} options={{headerShown: false}} />
+        <Stack.Screen name="List Providers" component={HomeScreen} options={{ headerShown: false }} />
         <Stack.Screen
-        name="Add Provider"
-        component={addProviderScreen}
-        options={({route}) => ({
-          title: route.params?.title,
-          headerStyle: {
-            backgroundColor: '#DFF8F7',
-          }
-        })}
-      />
+            name="Add Provider"
+            component={addProviderScreen}
+            options={({ route }) => ({
+                title: route.params?.title,
+                headerStyle: {
+                    backgroundColor: '#DFF8F7',
+                }
+            })}
+        />
     </Stack.Navigator>
 );
 export default function TabNavigator() {
     return (
-    <tab.Navigator
-    screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-        let iconName;
-        if (route.name == "Providers") { iconName = "home-outline"; }
-        else if (route.name == "Articles") { iconName = "phone-portrait-sharp"; }
-        else if (route.name == "News") { iconName = "newspaper-outline"; }
-        return (
-        <Ionicons
-        name={iconName}
-        color={color}
-        size={size}
-        />
-        );
-        },
-        })}>
-    <tab.Screen name="Providers" component={HomeStack} options={{ title: 'Providers' }}/>
-    <tab.Screen name="Articles" component={ArticleScreen} options={{ title: 'Articles' }}/>
-    <tab.Screen name="News" component={NewScreen} options={{ title: 'News' }}/>
-    </tab.Navigator>
+        <tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+                    if (route.name == "Providers") { iconName = "home-outline"; }
+                    else if (route.name == "Articles") { iconName = "phone-portrait-sharp"; }
+                    else if (route.name == "News") { iconName = "newspaper-outline"; }
+                    return (
+                        <Ionicons
+                            name={iconName}
+                            color={color}
+                            size={size}
+                        />
+                    );
+                },
+            })}>
+            <tab.Screen name="Providers" component={HomeStack} options={{ title: 'Providers' }} />
+            <tab.Screen name="Articles" component={ArticleScreen} options={{ title: 'Articles' }} />
+            <tab.Screen name="News" component={NewScreen} options={{ title: 'News' }} />
+        </tab.Navigator>
     );
-    }
-    const styles = StyleSheet.create({
+}
+const styles = StyleSheet.create({
     container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-    });
+});
--- amsMobile/screens/AddProviderScreen.js
+++ amsMobile/screens/AddProviderScreen.js
@@ -1,18 +1,92 @@
-import React, {  useState,useEffect } from 'react'
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  FlatList,TouchableOpacity
-} from 'react-native'
-const  addProviderScreen = () => {
-  return (
-    <View style={{ flex: 1 }}>
-    <Text>Hello Add Provider</Text>
- </View>
-  )
-}
+import React, { useState } from 'react';
+import { View, Text, Image, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import axios from "axios";
+import { useNavigation } from '@react-navigation/native'
+const AddProviderScreen = () => {
+    const [name, setName] = useState("");
+    const [adress, setAdress] = useState("");
+    const [email, setEmail] = useState("");
+    const navigation = useNavigation("");
+    const addProvider = async () => {
+        const provider = {
+            name: name,
+            adress: adress,
+            email: email,
+        };
+        try {
+            const response = await axios.post("https://ams.smart-it-partner.com/api/providers", provider, {
+                headers: { 'Content-Type': 'application/ld+json' }
+            });
+            //navigation.navigate('List Providers');
+             navigation.reset({ index: 0, routes: [{ name: 'List Providers', params: { refresh: true } }], });
+        } catch (error) {
+            // Gérez l'erreur ici
+            console.error(error);
+        }
+    };
+    return (
+        <View style={styles.container}>
+
+            <View style={styles.form}>
+                <Text style={styles.label}>Name</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Enter Name"
+                    value={name}
+                    onChangeText={setName}
+                />
+                <Text style={styles.label}>Email</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Enter Email"
+                    value={email}
+                    onChangeText={setEmail}
+                />
+                <Text style={styles.label}>Adress</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Enter Adress"
+                    value={adress}
+                    onChangeText={setAdress}
+                />
+                <TouchableOpacity style={styles.button} onPress={() => addProvider()}>
+                    <Text style={styles.buttonText}>Valider</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
 const styles = StyleSheet.create({
-})
-export default addProviderScreen;
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    form: {
+        width: '80%',
+    },
+    label: {
+        marginTop: 20,
+        marginBottom: 5
+    },
+    input: {
+        borderColor: '#ccc',
+        borderWidth: 1,
+        borderRadius: 5,
+        padding: 10,
+        fontSize: 18,
+    },
+    button: {
+        marginTop: 20,
+        backgroundColor: '#07B0A8',
+        borderRadius: 5,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 18,
+        textAlign: 'center',
+    },
+});
+export default AddProviderScreen;
