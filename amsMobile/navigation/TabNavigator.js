027ccb023805498a5dfaf92d2645a4ddf93d47cc
moahmed amine mezghich <git@mezghich.com>
2023-12-22 12:10:13 +0100

    drawer navigator

--- amsMobile/navigation/TabNavigator.js
+++ amsMobile/navigation/TabNavigator.js
@@ -1,24 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,TouchableOpacity } from 'react-native'
+import {
+    StyleSheet,
+    Button, TouchableOpacity
+} from 'react-native'
+
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
 import NewScreen from '../screens/NewScreen';
 import ArticleScreen from '../screens/ArticleScreen';
 import addProviderScreen from '../screens/AddProviderScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useNavigation } from '@react-navigation/native'
-
+import { useNavigation } from '@react-navigation/native';
 import authtoken from '../service/authtoken';
-
-
 const tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
-
 const HomeStack = () => (
     <Stack.Navigator>
-        <Stack.Screen name="List Providers" component={HomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="List Providers" component={HomeScreen} options={{
+
+            headerShown: false
+        }} />
         <Stack.Screen
             name="Add Provider"
             component={addProviderScreen}
@@ -27,52 +32,64 @@ const HomeStack = () => (
                 headerStyle: {
                     backgroundColor: '#DFF8F7',
                 }
+
             })}
         />
+       
     </Stack.Navigator>
 );
 export default function TabNavigator() {
     const navigation=useNavigation();
     const logout = () => {
-        authtoken.logout();
-        navigation.navigate('Login');
-      };
-      const LogoutButton = () => (
-        <TouchableOpacity onPress={logout} style={{ marginRight: 10 }}>
-          <Ionicons name="log-out-outline" size={25} color="#000" />
-        </TouchableOpacity>
-      );
-
-      
-
-    return (
-        <tab.Navigator
-            screenOptions={({ route }) => ({
-                tabBarIcon: ({ focused, color, size }) => {
-                    let iconName;
-                    if (route.name == "Providers") { iconName = "home-outline"; }
-                    else if (route.name == "Articles") { iconName = "phone-portrait-sharp"; }
-                    else if (route.name == "News") { iconName = "newspaper-outline"; }
-                    return (
-                        <Ionicons
-                            name={iconName}
-                            color={color}
-                            size={size}
-                        />
-                    );
-                },
-            })}>
-            <tab.Screen name="Providers" component={HomeStack} options={{ title: 'Providers', headerRight: () => <LogoutButton /> }} />
-            <tab.Screen name="Articles" component={ArticleScreen} options={{ title: 'Articles', headerRight: () => <LogoutButton /> }} />
-            <tab.Screen name="News" component={NewScreen} options={{ title: 'News', headerRight: () => <LogoutButton /> }} />
-        </tab.Navigator>
+      authtoken.logout();
+      navigation.openDrawer()
+    };
+    const LogoutButton = () => (
+      <TouchableOpacity onPress={logout} style={{ marginRight: 10 }}>
+        <AntDesign name="bars" size={25} color="#000" />
+      </TouchableOpacity>
     );
-}
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+   
+      return (
+  
+      <tab.Navigator
+      screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name == "Providers") { iconName = "home-outline"; }
+          else if (route.name == "Articles") { iconName = "phone-portrait-sharp"; }
+          else if (route.name == "News") { iconName = "newspaper-outline"; }
+          return (
+          <Ionicons
+          name={iconName}
+          color={color}
+          size={size}
+          />
+          );
+          },
+          })}>
+      <tab.Screen name="Providers" component={HomeStack}   options={{
+      title: 'Providers',
+      headerRight: () => <LogoutButton />
+    }}/>
+      <tab.Screen name="Articles" component={ArticleScreen} options={{
+      title: 'Articles',
+      headerRight: () => <LogoutButton />
+    }}/>
+      <tab.Screen name="News" component={NewScreen} options={{
+      title: 'News',
+      headerRight: () => <LogoutButton />
+    }}/>
+      </tab.Navigator>
+   
+      );
+      }
+      const styles = StyleSheet.create({
+      container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      },
+      });
+  
--- amsMobile/navigation/index.js
+++ amsMobile/navigation/index.js
@@ -5,16 +5,15 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SignInScreen from '../screens/SignInScreen';
 import SignUpScreen from '../screens/SignUpScreen';
 import TabNavigator from './TabNavigator';
+import DrawerNavigation from './DrawerNavigation';
 const Stack = createNativeStackNavigator();
-
 const Navigation = () => {
   return (
     <NavigationContainer>
      <Stack.Navigator screenOptions={{headerShown:false}}>
         <Stack.Screen name="Login" component={SignInScreen} />
         <Stack.Screen name="Register" component={SignUpScreen} />
-        {/*<Stack.Screen name="Home" component={HomeScreen} />*/}
-        <Stack.Screen name="Home" component={TabNavigator} />
+        <Stack.Screen name="Home" component={DrawerNavigation} />
       </Stack.Navigator>
   </NavigationContainer>
   )
@@ -22,7 +21,7 @@ const Navigation = () => {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor:'#F8FEFE'    
+      backgroundColor:'#F8FEFE'
     },
   });
 export default Navigation
